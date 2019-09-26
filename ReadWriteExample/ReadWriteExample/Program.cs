a97c3e8d16ddddae503db8f0eae120e5ff03fd01
KonstaEd <43011849+KonstaEd@users.noreply.github.com>
2019-09-26 13:25:40 +0300

    Tulostus vaihtoehdot

--- ReadWriteExample/ReadWriteExample/Program.cs
+++ ReadWriteExample/ReadWriteExample/Program.cs
@@ -6,7 +6,13 @@ namespace ReadWriteExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ReadWriteExample");
+            Console.WriteLine("ReadWriteExample"); // Vakioviesti
+            Console.Write("Moi, kirjoita nimesi: "); // Vakioviesti
+            string userInput = Console.ReadLine(); // String sijoitetaan muuttuujaan userInput
+            Console.WriteLine("-------------------------------------"); // Vakioviesti
+            Console.WriteLine("Moi " + userInput + "!"); // Vakioviesti + arvo userInput
+            Console.WriteLine("Moi {0}!", userInput); // Vakioviesti + arvo userInput (eri muoto)
+            Console.WriteLine($"Moi {userInput}!"); // Tulostuksen muotoilu ja muuttujien käyttö tulostuksen yhteydessä $-merking avulla = string.Format()
         }
     }
 }
