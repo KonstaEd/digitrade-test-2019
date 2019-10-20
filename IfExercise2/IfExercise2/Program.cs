2d0c9417bcfdb808fd2580edbe5feef63f866407
KonstaEd <43011849+KonstaEd@users.noreply.github.com>
2019-10-20 21:02:52 +0300

    2

---
+++ IfExercise2/IfExercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Syötä numero");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0) Console.WriteLine("Numero " + number + " on parillinen");
+            else
+            {
+                Console.WriteLine("Numero " + number + " on pariton");
+            }
+
+        }
+    }
+}
