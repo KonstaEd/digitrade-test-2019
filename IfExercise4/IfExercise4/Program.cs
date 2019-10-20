d3c5bd37915b97905e1b6cc64b1e1478ab7f4a4f
KonstaEd <43011849+KonstaEd@users.noreply.github.com>
2019-10-20 23:56:04 +0300

    4

---
+++ IfExercise4/IfExercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä 3 lukua");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            {
+                int[] n = new int[3] { a, b, c };
+                {
+                    Array.Sort(n);
+                    foreach (int value in n)
+                    {
+                        Console.Write(value + " ");
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
