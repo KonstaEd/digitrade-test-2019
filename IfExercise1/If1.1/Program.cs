e5f098aef46b85b3f0521a69466fadb4b5e44305
KonstaEd <43011849+KonstaEd@users.noreply.github.com>
2019-10-20 21:02:29 +0300

    1

---
+++ IfExercise1/If1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IfExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Syötä numero");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0) Console.WriteLine("Numero " + number + " on nolla");
+            else if (number < 0) Console.WriteLine("Numero " + number + " on negatiivinen");
+            else if (number > 0) Console.WriteLine("Numero " + number + " positiivinen");
+        }
+    }
+}
