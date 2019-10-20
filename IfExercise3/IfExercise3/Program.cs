fc9fc4f608591a6001d35f882dee4fa1384a1632
KonstaEd <43011849+KonstaEd@users.noreply.github.com>
2019-10-20 21:03:12 +0300

    3

---
+++ IfExercise3/IfExercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IfExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Syötä numero");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0) Console.WriteLine("Numero " + number + " on nolla");
+            else if (number < 0)
+            {
+                if (number % 2 == 0) Console.WriteLine("Numero " + number + " on negatiivinen ja parillinen");
+                else
+                {
+                    Console.WriteLine("Numero " + number + " on negatiivinen ja pariton");
+                }
+            }
+            else if (number > 0)
+            {
+                if (number % 2 == 0) Console.WriteLine("Numero " + number + " on poistiivinen ja parillinen");
+                else
+                {
+                    Console.WriteLine("Numero " + number + " on positiivinen ja pariton");
+                }
+
+            }
+        }
+    }
+}
