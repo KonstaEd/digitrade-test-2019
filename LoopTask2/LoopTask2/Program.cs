67e1529ecb216bd7fbd379e6d0da7c3af5679453
KonstaEd <43011849+KonstaEd@users.noreply.github.com>
2019-11-03 20:32:36 +0200

    2

---
+++ LoopTask2/LoopTask2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LoopTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        qwerty:
+            Console.WriteLine("Syötä positiivinen kokonaisluku");
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+
+            if (a > 0)
+            {
+                for (int i = 0; i <= a; i++)
+                {
+                    b = b + i;
+                }
+                Console.WriteLine($"Luvun tekijöiden summa on: {b}");
+
+            }
+            else
+            {
+                Console.WriteLine("Luku ei ole positiivinen kokonaisluku");
+                goto qwerty;
+            }
+        }
+    }
+}
